6fa59aeeb34867b3b5df64874accd1f84c4fe436
roshanpatil2000 <roshanpatil2912.rp@gmail.com>
2026-04-13 11:49:02 +0530

    added logger for api

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { LoggerInterceptor } from './common/interceptors/logger.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{
+    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
+  });
+
+  app.useGlobalInterceptors(new LoggerInterceptor());
+  
   await app.listen(process.env.PORT || 3000);
 
   Logger.log(`\n\nServer is running on http://localhost:${process.env.PORT || 3000}\n\n`);
