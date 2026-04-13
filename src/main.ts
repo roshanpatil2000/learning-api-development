0ef214d7bb78a2655c74dcb7edfb3659be81de27
roshanpatil2000 <roshanpatil2912.rp@gmail.com>
2026-04-13 11:35:17 +0530

    init nest project

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT || 3000);
+
+  Logger.log(`\n\nServer is running on http://localhost:${process.env.PORT || 3000}\n\n`);
+}
+bootstrap();
